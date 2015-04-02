b5edd069118223a1ec3e3b9201434e375bcb895f
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-02 21:22:33 +1100

    General structure coming together

---
+++ index.js
@@ -0,0 +1,42 @@
+var http = require('http');
+var url = require('url');
+
+var kgo = require('kgo');
+var out = require('out');
+var Router = require('routes');
+
+module.exports = function(opts) {
+  var router = new Router();
+  var server = http.createServer(handleRequest);
+  
+  function handleRequest(req, res) {
+    var path = url.parse(req.url).pathname;
+    var match = router.match(path);
+    
+    // run the match
+    (match && match.fn(req, res, match)) || notFound(req, res);
+  }
+  
+  function notFound(req, res) {
+    res.statusCode = 404;
+    res.end('not found');
+  }
+  
+  server.start = function() {
+    server.listen((opts || {}).port || 0, function(err) {
+      if (err) {
+        return out.error(err);
+      }
+      
+      out('workshop available @ http://localhost:{0}', server.address().port);
+    });
+  };
+  
+  kgo
+  (opts || {})
+  ('load-exercises', ['exercises'], require('./lib/load-exercises'))
+  ('create-routes', ['load-exercises'], require('./lib/create-routes')(router))
+  
+  
+  return server;
+};
